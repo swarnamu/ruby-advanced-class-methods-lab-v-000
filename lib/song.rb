560743869ebb80c6d3b627a0a46009e4bf15303e
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-01-30 21:02:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,8 @@ class Song
     self.all.find {|s| s.name == song_name}
   end
 
+  def self.find_or_create_by_name
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
 end
